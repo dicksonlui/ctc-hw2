69d9bb6fa5c9b50e5b207d30cc3f33078dc6e522
Dickson Lui <dicksonlui@berkeley.edu>
2014-03-04 02:26:40 -0800

    sup

--- index.js
+++ index.js
@@ -5,9 +5,41 @@ $(document).ready(function(){
 
  //QUESTION 3
 
+ $(".secondbutton").click(function(){
+    $(".secondheading").html("Shiba Inus (yes my favorite animals are Shiba Inus) are the best!!!")
+    				   .css('color', '#34DDDD');
+ });
+
  //QUESTION 4
+
+ $("#task4").click(function(){
+    $("#hipcat").css('-webkit-filter', 'blur(5px)');
+ });
 
  //QUESTION 5
 
+ $(".button").click(function(){
+ 	//Why does Chrome start the wheels as running even though the default state in style.css is paused?
+ 	$(".wheel").css('-webkit-animation-play-state', 'running');
+ 	$("#wheel1").css('-webkit-animation-play-state', 'running');
+ 	$("#wheel2").css('-webkit-animation-play-state', 'running');
+ });
+
  //QUESTION 6
+ $(".nyancat").click(function() {
+ 	$(".nyancat").animate({
+  	left: "+=1200"
+  }, 10000, function() {
+  	//Animation complete.
+  });
+
+  $(".rainbows").animate({
+  	left: "0",
+  	width: "+=1200"
+  }, 10000, function() {
+  	//Animation complete.
+  });
+
+ });
+
 });
